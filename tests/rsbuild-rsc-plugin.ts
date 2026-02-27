36b70ba68c0d284b1ed5faf54c8e9d1d007996b2
Cong-Cong <dacongsama@live.com>
2026-02-27 11:22:05 +0800

    ci rsbuild-rsc-plugin

--- tests/rsbuild-rsc-plugin.ts
+++ tests/rsbuild-rsc-plugin.ts
@@ -5,7 +5,6 @@ export async function test(options: RunOptions) {
 	await runInRepo({
 		...options,
 		repo: 'rstackjs/rsbuild-plugin-rsc',
-		branch: process.env.RSBUILD_REF ?? 'main',
 		beforeTest: async () => {
 			await $`npx playwright install`
 		},
